f3aba05b1aeb573b384156fa258736e30033adea
Srdjan Filipovic <srdjanfilipovic80@gmail.com>
2024-11-11 15:16:01 +0100

    #14 Napravljena AppPreference klasa koja omogucava lakse upravljanje podacima koji se cuvaju u shared preference

--- app/src/main/java/com/app/parkfinder/MainActivity.kt
+++ app/src/main/java/com/app/parkfinder/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.app.parkfinder.logic.AppPreferences
 import com.app.parkfinder.ui.activities.NavigationActivity
 import com.app.parkfinder.ui.activities.WelcomeActivity
 import com.auth0.android.jwt.JWT
@@ -13,18 +14,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val sharedPreferences = getSharedPreferences("auth_prefs", MODE_PRIVATE)
-        val accessToken = sharedPreferences.getString("access_token", null)
-        val refreshToken = sharedPreferences.getString("refresh_token", null)
-
+        AppPreferences.setup(applicationContext)
+        val accessToken = AppPreferences.accessToken
+        val refreshToken = AppPreferences.refreshToken
         Logger.getLogger("MainActivity").info("Access token: $accessToken")
 
         if(accessToken != null && !isTokenExpired(accessToken)) {
             // User is logged in and token is not expired
             Logger.getLogger("MainActivity").info("User is logged in")
-            val intent = Intent(this, NavigationActivity::class.java)
-            val options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_right, R.anim.slide_out_left)
-            startActivity(intent, options.toBundle())
+            navigateToNavigationPage()
         } else if(refreshToken != null) {
             // Optionally, use the refresh token to get a new access token
             navigateToLogin()   // TODO: Implement refresh token logic
@@ -35,6 +33,12 @@ class MainActivity : ComponentActivity() {
 
     private fun navigateToLogin() {
         val intent = Intent(this, WelcomeActivity::class.java)
+        val options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_right, R.anim.slide_out_left)
+        startActivity(intent, options.toBundle())
+    }
+
+    private fun navigateToNavigationPage(){
+        val intent = Intent(this, NavigationActivity::class.java)
         val options = ActivityOptions.makeCustomAnimation(this, R.anim.slide_in_right, R.anim.slide_out_left)
         startActivity(intent, options.toBundle())
     }
