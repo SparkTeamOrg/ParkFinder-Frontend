b36c3ea74675da26813616a2c1f8169f7096cb45
Srdjan Filipovic <srdjanfilipovic80@gmail.com>
2024-11-20 21:30:07 +0100

    #23 Dodato brisanje refresh tokena prilikom log outa korisnika

--- app/src/main/java/com/app/parkfinder/logic/services/TokenService.kt
+++ app/src/main/java/com/app/parkfinder/logic/services/TokenService.kt
@@ -4,11 +4,18 @@ import com.app.parkfinder.logic.models.BackResponse
 import com.app.parkfinder.logic.models.dtos.TokenDto
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface TokenService {
     @POST("token/refresh")
     suspend fun refresh(
         @Body refresh: TokenDto
     ): Response<BackResponse<TokenDto>>
+
+    @DELETE("token/delete")
+    suspend fun delete(
+        @Query("userId") userId: Number
+    ): Response<BackResponse<String>>
 }
